3b68c3c3845f817dc165447cc4a51716e94ba587
Danny Barash <dannybarash7@gmail.com>
2023-11-29 08:03:26 +0200

    m

--- zero_shot_segmentation/test_iou_of_zero_shot_with_prompts.py
+++ zero_shot_segmentation/test_iou_of_zero_shot_with_prompts.py
@@ -33,9 +33,15 @@ import os
 # Define the Roboflow project URL and API key
 rf_api_key= "R04BinsZcBZ6PsfKR2fP"
 rf_workspace= "yolab-kmmfx"
-rf_project_name = "11-16-2023-zero-shot-oct"
+rf_project_name = "11-24-2023-zero-shot"
 rf_dataset_type = "coco-segmentation" #"png-mask-semantic"
-version = 3
+version = 1
+CHECKPOINT_PATH = "/Users/dannybarash/Code/oct/zero_shot_segmentation_test_sam/weights/sam_vit_h_4b8939.pth"  # os.path.join("weights", "sam_vit_h_4b8939.pth")
+annot_dataset_dir = "/Users/dannybarash/Code/oct/zero_shot_segmentation_test_sam/zero_shot_segmentation/11/24/2023-zero-shot-1/test"
+raw_oct_dataset_dir = "/Users/dannybarash/Library/CloudStorage/GoogleDrive-dannybarash7@gmail.com/Shared drives/Yolab - Current Projects/Yonatan/Hist Images/"
+real_histology_dir = raw_oct_dataset_dir
+
+
 #roboflow semantic classes
 EPIDERMIS = True #mask values for epidermis mask
 
@@ -87,16 +93,13 @@ dataset = download_images_and_masks(rf_api_key, rf_workspace, rf_project_name, r
 # prepare model
 DEVICE = torch.device('mps')  # 'cpu'
 MODEL_TYPE = "vit_h"
-CHECKPOINT_PATH = "/Users/dannybarash/Code/oct/zero_shot_segmentation_test_sam/weights/sam_vit_h_4b8939.pth"  # os.path.join("weights", "sam_vit_h_4b8939.pth")
 sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT_PATH).to(device=DEVICE)
 predictor = SamPredictor(sam)
 
 
 total_iou = 0
 total_samples = 0
-annot_dataset_dir = "/Users/dannybarash/Code/oct/zero_shot_segmentation_test_sam/zero_shot_segmentation/11/16/2023-Zero-shot-OCT-3/test/"
-raw_oct_dataset_dir = "/Users/dannybarash/Library/CloudStorage/GoogleDrive-dannybarash7@gmail.com/Shared drives/Yolab - Current Projects/Yonatan/Hist Images/"
-real_histology_dir = raw_oct_dataset_dir
+
 # Get the list of image files
 image_files = [f for f in os.listdir(annot_dataset_dir) if f.endswith(".jpg")]
 image_files = image_files[:2]
--- zero_shot_segmentation/zero_shot_utils/predict_mask_on_oct_interactive.py
+++ zero_shot_segmentation/zero_shot_utils/predict_mask_on_oct_interactive.py
@@ -1,11 +1,40 @@
 
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 from OCT2Hist_UseModel.utils.crop import crop_oct
 from OCT2Hist_UseModel.utils.masking import get_sam_input_points, show_points, show_mask, mask_gel_and_low_signal
 from OCT2Hist_UseModel import oct2hist
 from zero_shot_segmentation.zero_shot_utils.run_sam_gui import run_gui_segmentation
+
+def warp_image(source_image, target_image, source_points, target_points):
+    # Convert the input points to NumPy arrays
+    src_pts = np.float32(source_points)
+    dst_pts = np.float32(target_points)
+
+    # Calculate the affine transformation matrix
+    affine_matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
+
+    # Apply the affine transformation to the source image
+    warped_image = cv2.warpPerspective(source_image, affine_matrix, (target_image.shape[1], target_image.shape[0]))
+
+    return warped_image
+
+def warp_oct(oct_image):
+    height,width,_ = oct_image.shape
+    first_row = oct_image[0, :, 0]
+    non_zero_indices = np.nonzero(first_row)[0]
+    x = [non_zero_indices[0],0] #0 stands for first row
+    y = [non_zero_indices[-1],0]  #0 stands for first row
+    last_row = oct_image[-1, :, 0]
+    non_zero_indices = np.nonzero(last_row)[0]
+    z = [non_zero_indices[0],height-1]
+    w = [non_zero_indices[-1],height-1]
+    source_points = np.float32([x,y,z,w])
+
+    target_points = np.float32([[0, 0], [width,0], [0,height-1], [width-1,height-1]])
+    result_image = warp_image(oct_image, oct_image, source_points, target_points)
 
 
 def predict(oct_input_image_path, predictor, weights_path, vhist = True):
@@ -15,8 +44,12 @@ def predict(oct_input_image_path, predictor, weights_path, vhist = True):
     # is it sheered?
     right_column = oct_image.shape[1] - 1
     if (oct_image[:, 0, 0] == 0).all() or (oct_image[:, right_column, 0] == 0).all():
-        print(f"{oct_input_image_path} is sheered, I can only segment full rectangular shapes.")
-        return None,None, None
+        oct_image = warp_oct(oct_image)
+        #assuming a trapezoidal shape, which touches the top and bottom rows.
+
+        # else:
+        # print(f"{oct_input_image_path} is sheered, I can only segment full rectangular shapes.")
+        # return None,None, None
     # OCT image's pixel size
     microns_per_pixel_z = 1
     microns_per_pixel_x = 1
