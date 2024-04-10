647b05ea62b4cb7eb0a5c23dd2b1bf3cec4028d1
Danny Barash <dannybarash7@gmail.com>
2024-04-10 23:50:18 +0300

    fixes for SAM

--- zero_shot_segmentation/test_iou_of_zero_shot_with_prompts.py
+++ zero_shot_segmentation/test_iou_of_zero_shot_with_prompts.py
@@ -44,7 +44,7 @@ rf_api_key= "R04BinsZcBZ6PsfKR2fP"
 rf_workspace= "yolab-kmmfx"
 rf_project_name = "paper_data"
 rf_dataset_type = "coco-segmentation" #"png-mask-semantic"
-version = 2
+version = 3
 
 if MEDSAM:
     CHECKPOINT_PATH = "/Users/dannybarash/Code/oct/medsam/MedSAM/work_dir/MedSAM/medsam_vit_b.pth"  # os.path.join("weights", "sam_vit_h_4b8939.pth")
