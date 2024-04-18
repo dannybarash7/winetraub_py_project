f868bc1c5f8db318b4fb23d3dd0b55b9356c1d9e
Danny Barash <dannybarash7@gmail.com>
2024-04-18 16:58:23 +0300

    moved nice with closing op to mask prediction

--- zero_shot_segmentation/consts.py
+++ zero_shot_segmentation/consts.py
@@ -6,7 +6,7 @@ rf_api_key = "R04BinsZcBZ6PsfKR2fP"
 rf_workspace = "yolab-kmmfx"
 rf_project_name = "paper_data"
 rf_dataset_type = "coco-segmentation"  # "png-mask-semantic"
-version = 5
+version = 6
 
 
 class COLORS:
