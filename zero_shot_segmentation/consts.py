5a46ad0ce5b628f55d411adc22236bab1c3b5823
Danny Barash <dannybarash7@gmail.com>
2024-07-30 18:16:30 +0300

    MedSam True in consts

--- zero_shot_segmentation/consts.py
+++ zero_shot_segmentation/consts.py
@@ -1,14 +1,14 @@
 import os
 
-MEDSAM = False
-SAMMED_2D = True
+MEDSAM = True
+SAMMED_2D = False
 SAM = False
 
 rf_api_key = "R04BinsZcBZ6PsfKR2fP"
 rf_workspace = "yolab-kmmfx"
-# rf_project_name = "paper_data"
-# rf_dataset_type = "coco-segmentation"  # "png-mask-semantic"
-# version = 7
+rf_project_name = "paper_data"
+rf_dataset_type = "coco-segmentation"  # "png-mask-semantic"
+version = 7
 
 INTERACTIVE_POINT_PREDICTION = True
 DOWNSAMPLE_SAM_INPUT = False
@@ -18,9 +18,11 @@ class COLORS:
     PREDICTED_EPIDERMISE_ORANGE = [255 / 255, 128 / 255, 0 / 255, 0.6]
     DONT_CARE = [255 / 255, 0 / 255, 0 / 255, 0.6]
 
-rf_project_name = "2024.4.30_83f_st2_cheek_10x_1_r2"
-rf_dataset_type = "coco-segmentation"  # "png-mask-semantic"
-version = 2
+# rf_project_name = "2024.4.30_83f_st2_cheek_10x_1_r2"
+# rf_dataset_type = "coco-segmentation"  # "png-mask-semantic"
+# version = 2
+
+#CONFIG
 TARGET_TISSUE_HEIGHT = 50
 ANNOTATED_DATA = True
 CONST_BOX = [0, 160, 1000, 400]
